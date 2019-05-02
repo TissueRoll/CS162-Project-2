bb9b653889af49a1064178435bb2b86438a428e6
TissueRoll <christopher.dizon@protonmail.com>
2019-05-03 01:28:20 +0800

    fixed some stuff ish

--- CS162A_Project2_Dizon_code.cpp
+++ CS162A_Project2_Dizon_code.cpp
@@ -10,10 +10,13 @@ int main() {
     int T, P, R; 
     cin >> T;
     for (int t = 1; t <= T; t++) {
-        memset(free_resources, 0, sizeof free_resources);
-        memset(held_resources, 0, sizeof held_resources);
-        memset(requested_resources, 0, sizeof requested_resources);
-        memset(finished, 0, sizeof finished);
+        // reset data
+        {
+            memset(free_resources, 0, sizeof free_resources);
+            memset(held_resources, 0, sizeof held_resources);
+            memset(requested_resources, 0, sizeof requested_resources);
+            memset(finished, 0, sizeof finished);
+        }
         // input
         {
             cin >> P >> R;
@@ -28,9 +31,9 @@ int main() {
                 for (int j = 0; j < R; j++)
                     cin >> requested_resources[i][j];
         }
+        // banker's algo
         vector<int> order;
         int still_requesting = P;
-        // banker's algo
         for (int p = 0; p < P; p++) {
             bool found = 0;
             for (int i = 0; i < P; i++) {
@@ -58,10 +61,26 @@ int main() {
             for (int i = 0; i < P; i++) 
                 if (not finished[i]) 
                     unfinished_processes.push_back(i);
-            cout << "DEADLOCKED PROCESSES: ";
-            for (int x : unfinished_processes) cout << x+1 << (x==unfinished_processes.back() ? '\n' : ' ');
-            cout << "TERMINATED ALL. RUNNING PROCESSES BY PROCESS ID.\n";
-            for (int u : unfinished_processes) order.push_back(u+1);
+            
+            cout << "DEADLOCKED PROCESSES:\n";
+            int most_progressed_process = -1;
+            int most_progressed_process_value = -1;
+            for (int x : unfinished_processes) {
+                cout << x+1 << (x==unfinished_processes.back() ? '\n' : ' ');
+                int temp_value = 0;
+                for (int i = 0; i < R; i++) temp_value += held_resources[x][i];
+                if (temp_value > most_progressed_process_value) {
+                    most_progressed_process = x;
+                    most_progressed_process_value = temp_value;
+                }
+            }
+            cout << "DEADLOCK STRATEGY: DEADLOCK RECOVERY.\n";
+            cout << "TERMINATED ALL BUT THE ONE WITH MOST RESOURCES HELD. RUNNING PROCESSES BY PROCESS ID.\n";
+            order.push_back(most_progressed_process+1);
+            for (int u : unfinished_processes) 
+                if (u != most_progressed_process) 
+                    order.push_back(u+1);
+            
         }
         cout << "PROCESS ORDER:\n";
         for (int x : order) cout << x << (x==order.back() ? '\n' : ' ');
